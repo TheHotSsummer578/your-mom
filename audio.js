f0034d8c80dbf4180f900b90cd3a6f052fe5ba02
TheHotSsummer578 <147924316+TheHotSsummer578@users.noreply.github.com>
2025-07-13 19:18:16 +0000

    commit 69

---
+++ audio.js
@@ -0,0 +1,15 @@
+const tracks = {
+  "ariamath": "https://github.com/shadersifyy/your-mom/raw/refs/heads/main/assets/mc_music/13%20Aria%20Math.mp3",
+  "lullaby": "https://github.com/shadersifyy/your-mom/raw/refs/heads/main/assets/mc_music/03%20Subwoofer%20Lullaby.mp3",
+  "moogcity2": "https://github.com/shadersifyy/your-mom/raw/refs/heads/main/assets/mc_music/06%20Moog%20City%202.mp3"
+};
+
+/*function play(trackName) {
+  const audio = new Audio(tracks[trackName]);
+  audio.autoplay = true;
+  audio.loop = true;
+  audio.preload = "auto";
+  audio.volume = 0.8; // optional
+  audio.play().catch(err => console.warn("Autoplay blocked:", err));
+}
+  */
